7d7c9da1b52f6c8fc662ebfadfa90d1e4deb0f06
ghost-mann <iamaob8@protonmail.com>
2026-07-28 10:29:12 +0300

    feat(branding): hero stat, category card and footer link child tables

---
+++ upande_webstore/upande_webstore/doctype/webstore_footer_link/webstore_footer_link.py
@@ -0,0 +1,5 @@
+from frappe.model.document import Document
+
+
+class WebstoreFooterLink(Document):
+	pass
---
+++ upande_webstore/upande_webstore/doctype/webstore_hero_stat/webstore_hero_stat.py
@@ -0,0 +1,5 @@
+from frappe.model.document import Document
+
+
+class WebstoreHeroStat(Document):
+	pass
